c9da7a7148a221eb2cc995e874cb9ecaa73414f6
Justin Purnell <jpurnell7996@gmail.com>
2016-09-12 09:37:43 -0400

    Initial Commit

---
+++ smpteCaptionCreator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  smpteCaptionCreator
+//
+//  Created by Justin Purnell on 9/12/16.
+//  Copyright © 2016 Justin Purnell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
